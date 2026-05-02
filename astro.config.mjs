d6b4a4bda3c233ec79ab7d0b795dee334421da1a
Johannes Grof <147989511+jx-grxf@users.noreply.github.com>
2026-05-02 11:52:52 +0200

    chore(config): enable vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://johannesgrof.me",
   devToolbar: {
     enabled: false,
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
